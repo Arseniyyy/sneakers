a4299f6de6186fbf460e730514b26c9df54c816f
arseniyyy <arskosh05@mail.ru>
2023-08-10 02:35:48 +0700

    Add CommonIcon to populate every icon with a parent class

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import cartIcon from 'icons/cart.svg'
-import profileIcon from 'icons/profile.svg'
+import { useState } from 'react'
+import CartIcon from 'components/icons/CartIcon'
 import useMediaQuery from 'hooks/useMediaQuery'
-import BurgerMenu from 'components/svgs/BurgerMenu'
-import XMarkIcon from 'components/svgs/XMarkIcon'
+import BurgerMenuIcon from 'components/icons/BurgerMenuIcon'
+import ProfileIcon from 'components/icons/ProfileIcon'
+import Sidebar from './Sidebar'
 import { svgTransitionColors } from 'styles'
 
 interface IProps {}
@@ -13,43 +13,41 @@ const Navbar = (props: IProps) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
 
-  const handleBurgerMenuClick = () => {
+  const handleMenuToggleClick = () => {
     setIsMenuToggled(!isMenuToggled)
   }
 
-  const defaultIconWidth = "35px"
-  const defaultIconHeight = "35px"
+  const defaultIconWidth = '35px'
+  const defaultIconHeight = '35px'
+
+  const profileIconWidth = '30px'
+  const profileIconHeight = '30px'
 
   return <div>
     <nav id="page-wrap" className="flex justify-between items-center p-8 bg-zinc-100 font-montserrat text-base font-medium text-white no-underline">
-      <img className="logo" style={{ width: 300, height: 100 }} src="logo.svg" />
+      <img className="logo" style={{ width: 210, height: 100 }} src="logo.svg" />
       {
         isAboveMediumScreens ?
-          <ul className="nav__links flex justify-between gap-4 mx-10">
+          <ul className="flex justify-between gap-4 mx-10">
             <button>
-              <img src={cartIcon} style={iconsStyles} alt="cart" />
+              <CartIcon width={defaultIconWidth} height={defaultIconHeight} />
             </button>
+
             <button>
-              <img src={profileIcon} style={iconsStyles} alt="profile" />
+              <ProfileIcon width={profileIconWidth} height={profileIconHeight} />
             </button>
           </ul>
 
           :
 
-          <div className="w-9 h-9 transition-all focus:rounded-md focus:border-dashed focus:border-green hover:scale-125 outline-0">
-            <button onClick={handleBurgerMenuClick}>
-              <BurgerMenu width="35px" height="35px" />
+          <div className="w-9 h-9 focus:rounded-md focus:border-dashed focus:border-green outline-0">
+            <button onClick={handleMenuToggleClick}>
+              <BurgerMenuIcon width="35px" height="35px" />
             </button>
           </div>
       }
-      {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
-          <div className="flex justify-end p-12">
-            <button className={svgTransitionColors} onClick={() => setIsMenuToggled(!isMenuToggled)}>
-              <XMarkIcon width={defaultIconWidth} height={defaultIconHeight} />
-            </button>
-          </div>
-        </div>
+      {!isAboveMediumScreens && (
+        <Sidebar isMenuToggled={isMenuToggled} iconWidth={defaultIconWidth} iconHeight={defaultIconHeight} onClick={handleMenuToggleClick} />
       )}
     </nav>
   </div>
--- src/components/navbar/Sidebar.tsx
+++ src/components/navbar/Sidebar.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { slide as Menu } from 'react-burger-menu'
+import XMarkIcon from 'components/icons/XMarkIcon'
 
 interface IProps {
-  pageWrapId: string
-  outerContainerId: string
+  iconWidth: string
+  iconHeight: string
+  isMenuToggled: boolean
+  onClick: () => void
 }
 
 const Sidebar = (props: IProps) => {
-
+  return <div className={`sidebar fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl ${props.isMenuToggled ? 'open' : 'close'}`}>
+    <div className="flex justify-end p-12">
+      <button onClick={props.onClick}>
+        <XMarkIcon width={props.iconWidth} height={props.iconHeight} />
+      </button>
+    </div>
+  </div>
 }
 
 export default Sidebar
